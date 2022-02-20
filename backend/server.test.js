6f110334e02cd325be4902aa3c3a54093f27be29
Richard Raita <48387692+ric-rai@users.noreply.github.com>
2022-02-20 20:36:36 +0200

    Add styling

--- backend/server.test.js
+++ backend/server.test.js
@@ -3,14 +3,17 @@ let mockList, list = {read: () => mockList, write: (c) => mockList = c}
 const server = require('./server.js')(list)
 const host = '127.0.0.1', port = 3001
 const url = `http://${host}:${port}`
+const grn = "\x1b[1;32m", cyan = "\x1b[1;36m", red = "\x1b[1;31m", reset = "\x1b[0m"
+const startMsg = msg => console.log(cyan + msg + reset)
+const successMsg = msg => console.log('✅ ', grn + msg + reset, '\n')
 
 const equal = (desc, ac, ex) => () => ac === ex ? true :
-    console.log(`${desc}:\nExpected: ${ex}\nActual: ${ac}`)
+    console.log(`${desc}:${grn}\nExpected: ${reset}${ex}\n${red}Actual: ${reset}${ac}`)
 const assert = (sMsg, ...assertions) => assertions.map(f => f()).every(v => v) ?
-    console.log(sMsg) : console.log()
+    successMsg(sMsg) : console.log()
 
 async function postTest(page) {
-    console.log('Testing POST: /coffee')
+    startMsg('Testing POST: /coffee')
     mockList = '[]'
     await page.setRequestInterception(true)
     const body = JSON.stringify({name: "Juhla Mokka", weight: 500, price: 5.79, roast: 1})
@@ -29,10 +32,10 @@ async function postTest(page) {
 }
 
 (async () => {
-    console.log('\nStarting server')
+    startMsg('\nStarting server')
     server.listen(port, host)
     console.log(`Listening at http://${host}:${port}`)
-    console.log('Server started')
+    successMsg('Server started')
     const browser = await puppeteer.launch()
     const postPage = await browser.newPage()
     await postTest(postPage)
